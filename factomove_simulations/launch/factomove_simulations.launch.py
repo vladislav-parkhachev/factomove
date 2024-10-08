a9b085ace9744fa2d4ab2b0b96c023344cc4c41d
Vladislav Parkhachev <vladparhachev777@gmail.com>
2024-10-09 02:19:37 +0300

    Update factomove_simulation.launch.py

--- factomove_simulations/launch/factomove_simulations.launch.py
+++ factomove_simulations/launch/factomove_simulations.launch.py
@@ -2,22 +2,53 @@ import os
 import xacro
 from pathlib import Path
 from ament_index_python.packages import get_package_share_directory
-
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
-from launch.actions import RegisterEventHandler, SetEnvironmentVariable
-from launch.event_handlers import OnProcessExit
+from launch.actions import IncludeLaunchDescription
+from launch.actions import SetEnvironmentVariable
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 def generate_launch_description():
 
     ld = LaunchDescription()
+
     gazebo_rosPackageLaunch = PythonLaunchDescriptionSource(os.path.join(get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py'))
+
     gazeboLaunch = IncludeLaunchDescription(gazebo_rosPackageLaunch, 
                     launch_arguments={'gz_args': ['-r -v -v4 empty.sdf'], 'on_exit_shutdown': 'true'}.items())
     
-    ld.add_action(gazeboLaunch)
+    factomove_description_path = os.path.join(get_package_share_directory('factomove_description'))
 
+    gazebo_resource_path = SetEnvironmentVariable(
+        name='GZ_SIM_RESOURCE_PATH',
+        value=[
+            str(Path(factomove_description_path).parent.resolve())
+            ]
+        )
+
+    xacro_file = os.path.join(factomove_description_path, 'urdf', 'factomove.urdf.xacro')
+
+    doc = xacro.process_file(xacro_file, mappings={'use_sim' : 'true'})
+
+    robot_desc = doc.toprettyxml(indent='  ')
+
+    params = {'robot_description': robot_desc}
+
+    start_gazebo_ros_spawner_cmd = Node(
+    package='ros_gz_sim',
+    executable='create',
+    arguments=[
+        '-name', 'factomove',
+        '-string', robot_desc,
+        '-x', '0.0',
+        '-y', '0.0',
+        '-z', '0.01'
+    ],
+    output='screen',
+)
+
+    ld.add_action(gazebo_resource_path)
+    ld.add_action(gazeboLaunch)
+    ld.add_action(start_gazebo_ros_spawner_cmd)
+    
     return ld
