a9c07a2190a3aa74922cb9049882597c57fc0ea4
Vladislav Parkhachev <vladparhachev777@gmail.com>
2024-11-08 01:53:54 +0300

    Add move in simulation

--- factomove_simulations/launch/factomove_simulations.launch.py
+++ factomove_simulations/launch/factomove_simulations.launch.py
@@ -1,30 +1,43 @@
 import os
-import xacro
-from pathlib import Path
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
-from launch.actions import SetEnvironmentVariable
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import SetEnvironmentVariable
+from pathlib import Path
+
 from launch_ros.actions import Node
+import xacro
 
 def generate_launch_description():
 
-    ld = LaunchDescription()
+    robotXacroName = 'factomove'
+
+    namePackageDescription = 'factomove_description'
+
+    namePackageSimulations = 'factomove_simulations'
+
+    modelFileRelativePath = 'urdf/factomove.urdf.xacro'
+
+    pathModelFile = os.path.join(get_package_share_directory(namePackageDescription), modelFileRelativePath)
+
+    robotDescription = xacro.process_file(pathModelFile).toxml()
 
     gazebo_rosPackageLaunch = PythonLaunchDescriptionSource(os.path.join(get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py'))
-
-    gazeboLaunch = IncludeLaunchDescription(gazebo_rosPackageLaunch, 
-                    launch_arguments={'gz_args': ['-r -v -v4 empty.sdf'], 'on_exit_shutdown': 'true'}.items())
     
-    factomove_description_path = os.path.join(get_package_share_directory('factomove_description'))
+    gazeboLaunch = IncludeLaunchDescription(gazebo_rosPackageLaunch, launch_arguments={'gz_args': ['-r -v -v4 empty.sdf'], 'on_exit_shutdown': 'true'}.items())
 
-    gazebo_resource_path = SetEnvironmentVariable(
-        name='GZ_SIM_RESOURCE_PATH',
-        value=[
-            str(Path(factomove_description_path).parent.resolve())
-            ]
-        )
+    spawnModelNodeGazebo = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=[
+            '-name', robotXacroName,
+            '-topic', 'robot_description'
+        ],
+        output='screen'
+    )
+
+    factomove_description_path = os.path.join(get_package_share_directory('factomove_description'))
 
     xacro_file = os.path.join(factomove_description_path, 'urdf', 'factomove.urdf.xacro')
 
@@ -35,20 +48,52 @@ def generate_launch_description():
     params = {'robot_description': robot_desc}
 
     start_gazebo_ros_spawner_cmd = Node(
-    package='ros_gz_sim',
-    executable='create',
-    arguments=[
-        '-name', 'factomove',
-        '-string', robot_desc,
-        '-x', '0.0',
-        '-y', '0.0',
-        '-z', '0.01'
-    ],
-    output='screen',
-)
+        package='ros_gz_sim',
+        executable='create',
+        arguments=[
+            '-name', 'factomove',
+            '-string', robot_desc,
+            '-x', '0.0',
+            '-y', '0.0',
+            '-z', '0.01'
+        ],
+        output='screen',
+    )
 
-    ld.add_action(gazebo_resource_path)
-    ld.add_action(gazeboLaunch)
-    ld.add_action(start_gazebo_ros_spawner_cmd)
+    nodeRobotStatePublisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description' : robotDescription, 'use_sim_time' : True}]
+    )
     
-    return ld
+    bridge_params = os.path.join(get_package_share_directory(namePackageSimulations), 'parameters', 'bridge_parameters.yaml')
+
+    start_gazebo_ros_bridge_cmd = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=[
+            "/cmd_vel" + "@geometry_msgs/msg/Twist" + "]gz.msgs.Twist",
+        ],
+        output='screen'
+    )
+
+    factomove_description_path = os.path.join(get_package_share_directory('factomove_description'))
+
+    gazebo_resource_path = SetEnvironmentVariable(
+        name='GZ_SIM_RESOURCE_PATH',
+        value=[
+            str(Path(factomove_description_path).parent.resolve())
+            ]
+    )
+    
+    launchDescriptionObject = LaunchDescription()
+
+    launchDescriptionObject.add_action(gazebo_resource_path)
+    launchDescriptionObject.add_action(gazeboLaunch)
+    launchDescriptionObject.add_action(start_gazebo_ros_spawner_cmd)
+    # launchDescriptionObject.add_action(spawnModelNodeGazebo)    
+    launchDescriptionObject.add_action(nodeRobotStatePublisher)
+    launchDescriptionObject.add_action(start_gazebo_ros_bridge_cmd)
+    
+    return launchDescriptionObject
